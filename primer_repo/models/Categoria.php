21357b8f599da68e3dbc228cd18031a8d5fc9caf
Julian Rojas <129390687+judali05@users.noreply.github.com>
2023-11-28 12:27:05 -0500

    Add files via upload

---
+++ primer_repo/models/Categoria.php
@@ -0,0 +1,14 @@
+<?php
+    class Categoria extends Conectar{
+
+        public function get_categoria(){
+            $conectar= parent::Conexion();
+            parent::set_names();
+                $sql="SELECT * FROM categorias WHERE est=1";
+                $sql=$conectar->prepare($sql);
+                $sql->execute();
+        
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
+?>
